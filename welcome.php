2c1c1abf9acf63aec1b546d225163aa5e2a609ab
draest <56384984+draest@users.noreply.github.com>
2019-10-12 22:47:32 +0530

    updating placeholder name

--- welcome.php
+++ welcome.php
@@ -33,7 +33,7 @@
 					</span>
 
                 <div class="wrap-input100 validate-input m-b-10" data-validate = "Username is required">
-                    <input class="input100" type="text" name="fname" placeholder="First Name" required>
+                    <input class="input100" type="text" name="fname" placeholder="" required>
                     <span class="focus-input100"></span>
                     <span class="symbol-input100">
 							<i class="fa fa-user"></i>
